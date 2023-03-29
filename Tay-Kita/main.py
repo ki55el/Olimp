fe9b2d9018b5cd867122732cfebd86f71c52baa7
Damir Syzdykov <125348732+ki55el@users.noreply.github.com>
2023-03-30 05:57:05 +0600

    Add files via upload

---
+++ Tay-Kita/main.py
@@ -0,0 +1,25 @@
+with open('Input.txt', 'r') as Inp:
+    tk = [w for w in Inp.readline().split()]
+Ans = [''] * len(tk)
+
+Start = int(len(tk)/2)
+for I in range(len(tk)):
+    if I % 2 == 0:
+        Dif = I
+    else:
+        Dif = -I
+    Start += Dif
+
+    start = int(len(tk[Start])/2)
+    for i in range(len(tk[Start])):
+        if i % 2 == 0:
+            dif = i
+        else:
+            dif = -i
+        start += dif
+
+        Ans[I] += tk[Start][start]
+
+with open('Output.txt', 'w') as Outp:
+    Outp.write(' '.join(Ans))
+    Outp.close()
