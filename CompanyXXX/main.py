e37499c26fe5147eac680859d13be7082d35fb37
Damir Syzdykov <125348732+ki55el@users.noreply.github.com>
2023-04-06 02:23:14 +0600

    Update main.py

--- CompanyXXX/main.py
+++ CompanyXXX/main.py
@@ -1,27 +1,43 @@
 empl = dict()
 names = dict()
+nums = dict()
 with open('Input.txt', 'r') as Inp:
     for count, line in enumerate(iter(Inp.readline, 'END\n')):
         if count % 2 == 0:
-            boss = line.strip().split(' ', 1)
-        else:
-            sub = line.strip().split(' ', 1)
             try:
-                empl[boss[0]].append(sub[0])
-            except KeyError:
-                empl[boss[0]] = [sub[0]]
+                boss, bossname = line.strip().split(' ', 1)
+                names[boss] = bossname
+                nums[bossname] = boss
+            except ValueError:
+                boss = line.strip()
+                if not(boss in names.keys()):
+                    names[boss] = 'Unknown Name'
+        else:
+            try:
+                sub, subname = line.strip().split(' ', 1)
+                names[sub] = subname
+            except ValueError:
+                sub = line.strip()
+                if not(sub in names.keys()):
+                    names[sub] = 'Unknown Name'
+            if boss in empl.keys():
+                empl[boss].append(sub)
+            else:
+                empl[boss] = [sub]
     BOSS = [Inp.readline()]
+if BOSS[0] in nums.keys():
+    BOSS = [nums[BOSS[0]]]
+
 ans = []
 while set(BOSS) & set(empl.keys()):
     res = []
-    for i in BOSS:
-        try:
-            ans.extend(empl[i])
-            res.extend(empl[i])
-        except KeyError:
-            pass
+    for t in BOSS:
+        if t in empl.keys():
+            ans.extend(empl[t])
+            res.extend(empl[t])
     BOSS = res
-print(ans)
+ans = sorted([f'{n} {names[n]}' for n in ans])
+if not ans:
+    ans = ['NO']
 with open('Output.txt', 'w') as Outp:
     Outp.write('\n'.join(ans))
-    Outp.close()
